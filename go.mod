ccab9c66780e52721bdbdfa34126dda19df84f98
shanedabes <shane@isda.best>
2021-11-26 22:43:55 +0000

    add kbday gowon module

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/gowon-irc/gowon-kbday
+
+go 1.17
+
+require (
+	github.com/anaskhan96/soup v1.2.4
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/gowon-irc/go-gowon v0.0.0-20211113200040-cb1a970bd247
+	github.com/jessevdk/go-flags v1.5.0
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ kbday.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/anaskhan96/soup"
+)
+
+func colourList(in []string) (out []string) {
+	colours := []string{"green", "red", "blue", "orange", "magenta", "cyan", "yellow"}
+	cl := len(colours)
+
+	for n, i := range in {
+		c := colours[n%cl]
+		o := fmt.Sprintf("{%s}%s{clear}", c, i)
+		out = append(out, o)
+	}
+
+	return out
+}
+
+func kbday() (string, error) {
+	t := time.Now()
+	tf := t.Format("January_02")
+
+	url := fmt.Sprintf("https://kpop.fandom.com/wiki/Template:%s", tf)
+
+	resp, err := soup.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	doc := soup.HTMLParse(resp)
+
+	bdays := []string{}
+	for _, li := range doc.Find("table").FindAll("li") {
+		bdays = append(bdays, li.FullText())
+	}
+
+	cl := colourList(bdays)
+
+	return fmt.Sprintf("Happy birthday to: %s", strings.Join(cl, ", ")), nil
+}
